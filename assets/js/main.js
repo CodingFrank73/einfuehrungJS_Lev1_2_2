42b0e199429ca91b0acb41965c6cd3138b354b59
Frank <codingfrank73@gmail.com>
2022-01-18 15:15:13 +0100

    init

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.getElementById("info").innerHTML = "<h1>Hello World</h1>";
+document.getElementById("info").innerHTML += "<h2>How are you?</h2>";
+
+document.getElementById("container").innerHTML = "<p>start of the element</p>";
+document.write("end of the element")
+
+
+document.getElementById("gallery").innerHTML = "<figure style='display: inline-block'><img src='assets/img/picture1.jpg' alt='pic1'><figcaption>Bild1</figcaption></figure><figure style='display: inline-block'><img src='assets/img/picture1.jpg' alt='pic2'><figcaption>Bild2</figcaption></figure><figure style='display: inline-block'><img src='assets/img/picture1.jpg' alt='pic3'><figcaption>Bild3</figcaption>"
